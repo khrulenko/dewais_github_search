ecf3e3d378c28197b8e1bd617a39409f4af1bb9e
Khrulenko Vitalii <khrulenko.v@gmail.com>
2023-08-05 23:51:15 +0300

    improved thunk functions

--- src/common/api.ts
+++ src/common/api.ts
@@ -1,39 +1,43 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { Octokit } from '@octokit/rest';
+import {
+  GetResponseDataTypeFromEndpointMethod,
+  RequestInterface,
+  RequestParameters,
+} from '@octokit/types';
 
 export const octokit = new Octokit({
   auth: process.env.REACT_APP_GITHUB_TOKEN,
 });
 
+const fetchData = async <T extends RequestInterface>(
+  method: GetResponseDataTypeFromEndpointMethod<T>,
+  params: RequestParameters
+) => {
+  try {
+    const usersResponse = await method(params);
+
+    return usersResponse.data;
+  } catch (error) {
+    throw new Error();
+  }
+};
+
 const searchUsersByLogin = createAsyncThunk(
   'search/searchUsers',
-  async (query: string) => {
-    try {
-      const usersResponse = await octokit.rest.search.users({
-        q: query,
-        per_page: 50,
-      });
-
-      return usersResponse.data;
-    } catch (error) {
-      throw new Error();
-    }
-  }
+  (query: string) =>
+    fetchData(octokit.rest.search.users, {
+      q: query,
+      per_page: 50,
+    })
 );
 
 const fetchUserData = createAsyncThunk(
-  'search/fetchUserData',
-  async (username: string) => {
-    try {
-      const usersResponse = await octokit.rest.users.getByUsername({
-        username,
-      });
-
-      return usersResponse.data;
-    } catch (error) {
-      throw new Error();
-    }
-  }
+  'search/searchUsers',
+  (username: string) =>
+    fetchData(octokit.rest.users.getByUsername, {
+      username,
+    })
 );
 
 export { searchUsersByLogin, fetchUserData };
