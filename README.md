e66d1a3f4402376723249e694ecb724c4e59f228
Khrulenko Vitalii <khrulenko.v@gmail.com>
2023-08-11 15:50:59 +0300

    added token to fetching

--- README.md
+++ README.md
@@ -16,10 +16,8 @@
   - toolkit
   - thunk
 - Material UI
-- Octokit.js (fetching GitHub data)
+- Octokit.js (fetching GitHub data, data types)
 
 ## UX description:
 
 A responsive and scalable single-page application (SPA) that allows users to search for GitHub users by their username. The search query is performed by clicking on the search button or pressing the Enter key. The search result is a list of found users in the form of cards with basic information. Each card can be clicked to see more detailed information, which will open a new route. If no user was found with the entered username or if an error occurred, the user will be notified with an appropriate message. The loading process is accompanied by visual loaders during the search and skeletons when loading cards with detailed information about the user.
-
-Pay attention: it's allowed to make up to 60 requests per hour. To be able to make more requests follow instructions from src/common/api.
--- src/common/api.ts
+++ src/common/api.ts
@@ -1,15 +1,19 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { Octokit } from '@octokit/rest';
-// import {
-//   GetResponseDataTypeFromEndpointMethod,
-//   RequestInterface,
-//   RequestParameters,
-// } from '@octokit/types';
 
-// public rest APIs allow to make up to 60 requests per hour
+const octokit = new Octokit({
+  auth: process.env.REACT_APP_GITHUB_TOKEN,
+});
+
+const config = {
+  headers: {
+    Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+  },
+};
+
 const fetchData = async (api: string) => {
   try {
-    const dataResponse = await fetch(api);
+    const dataResponse = await fetch(api, config);
 
     if (!dataResponse.ok) {
       throw new Error();
@@ -35,50 +39,5 @@ const fetchUserData = createAsyncThunk(
   'search/fetchUserData',
   (username: string) => fetchData(`https://api.github.com/users/${username}`)
 );
-
-const octokit = new Octokit({
-  auth: process.env.REACT_APP_GITHUB_TOKEN,
-});
-
-// @
-// Octokit.js allows to make up to 5000 requests per hour
-// to use Octokit requests, follow the instructions below:
-// 1. Create a GitHub token on your GitHub profile
-// 2. Create a .env file in the repository root
-// 3. Add the token to the .env file using REACT_APP_GITHUB_TOKEN name
-// 4. Uncomment fetchData, searchUsersByLogin and fetchUserData funcs below
-// 5. Uncomment the octokit/types import above
-// 6. Comment fetchData, searchUsersByLogin and fetchUserData above
-// @
-
-// const fetchData = async <T extends RequestInterface>(
-//   method: GetResponseDataTypeFromEndpointMethod<T>,
-//   params: RequestParameters
-// ) => {
-//   try {
-//     const usersResponse = await method(params);
-
-//     return usersResponse.data;
-//   } catch (error) {
-//     throw new Error();
-//   }
-// };
-
-// const searchUsersByLogin = createAsyncThunk(
-//   'search/searchUsers',
-//   (query: string) =>
-//     fetchData(octokit.rest.search.users, {
-//       q: query,
-//       per_page: 50,
-//     })
-// );
-
-// const fetchUserData = createAsyncThunk(
-//   'search/fetchUserData',
-//   (username: string) =>
-//     fetchData(octokit.rest.users.getByUsername, {
-//       username,
-//     })
-// );
 
 export { octokit, searchUsersByLogin, fetchUserData };
