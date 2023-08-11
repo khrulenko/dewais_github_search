40fa5ce83b5149b25ee6ff1ff30ce4eb79fc784c
Khrulenko Vitalii <khrulenko.v@gmail.com>
2023-08-11 16:27:06 +0300

    updated APIs

--- src/common/api.ts
+++ src/common/api.ts
@@ -5,15 +5,26 @@ const octokit = new Octokit({
   auth: process.env.REACT_APP_GITHUB_TOKEN,
 });
 
-const config = {
-  headers: {
-    Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-  },
-};
+// @
+// APIs allow to make up to 60 requests per hour
+// to be able to make up to 5000 requests, follow the instructions below:
+// 1. Create a GitHub token on your GitHub profile
+// 2. Create an ".env" file in the repository root
+// 3. Add the token to the ".env" file using "REACT_APP_GITHUB_TOKEN" name
+// 4. Uncomment "config" const below
+// 5. Add "config" const as a second argument
+//    to the fetch function call into "fetchData" function
+// @
+
+// const config = {
+//   headers: {
+//     Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+//   },
+// };
 
 const fetchData = async (api: string) => {
   try {
-    const dataResponse = await fetch(api, config);
+    const dataResponse = await fetch(api);
 
     if (!dataResponse.ok) {
       throw new Error();
